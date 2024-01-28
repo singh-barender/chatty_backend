dcf3b91fe1b7d2cf65f11cad8a0a1f039a232945
Barender <b1h.gagandeep@gmail.com>
2024-01-28 16:40:50 +0530

    add: added some schemes in auth

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Application } from 'express';
 
 // const BASE_PATH = '/api/v1';
 
-export default (app: Application) => {
+export default (_app: Application) => {
   const routes = () => {};
   routes();
 };

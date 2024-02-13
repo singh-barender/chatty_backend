aec8c63596462bcb900793af74dc1704d104c3b7
Barender <b1h.gagandeep@gmail.com>
2024-02-14 00:11:00 +0530

    feat: added signin and signout feature

--- src/features/auth/routes/authRoutes.ts
+++ src/features/auth/routes/authRoutes.ts
@@ -1,5 +1,8 @@
-import { SignUp } from '@auth/controllers/signup';
 import express, { Router } from 'express';
+// import { Password } from '@auth/controllers/password';
+import { SignIn } from '@auth/controllers/signin';
+import { SignOut } from '@auth/controllers/signout';
+import { SignUp } from '@auth/controllers/signup';
 
 class AuthRoutes {
   private router: Router;
@@ -10,6 +13,15 @@ class AuthRoutes {
 
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
+    this.router.post('/signin', SignIn.prototype.read);
+    // this.router.post('/forgot-password', Password.prototype.create);
+    // this.router.post('/reset-password/:token', Password.prototype.update);
+
+    return this.router;
+  }
+
+  public signoutRoute(): Router {
+    this.router.get('/signout', SignOut.prototype.update);
 
     return this.router;
   }
